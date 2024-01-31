25bb5d59e61ebf2776b4f1af1729cfcd6e91cb9b
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-01-31 12:13:36 +0545

    'test'

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -19,7 +21,14 @@ authService.getCurrentUser().then((userData) => {
 }).finally(() => setLoading(false))
 }, [])
   
- return !loading ? (<div className=''></div>) : null
+ return !loading ? (
+ <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+  <div className='w-full block'><Header />
+  <main>
+   TODO <Outlet />
+  </main>
+  <Footer />
+  </div></div>) : null
 }
 
 export default App
