57fda47a000fe174ee5a2c3e193cbb2103df02ef
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-01-31 11:59:27 +0545

    'tailwindInstallation'

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,15 +1,25 @@
-
+import {useDispatch} from 'react-redux'
+import { useState, useEffect } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+const dispatch = useDispatch()
 
-  return (
+useEffect(( )=> {
+authService.getCurrentUser().then((userData) => {
+  if (userData){
+    dispatch(login({userData}))
+  }
+  else {
+    dispatch(logout())
+  }
+}).finally(() => setLoading(false))
+}, [])
   
-
-    <>
-      <h1>A blog</h1>
-    </>
-  )
+ return !loading ? (<div className=''></div>) : null
 }
 
 export default App
