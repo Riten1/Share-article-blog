c409a89a172cd1e0643b51b21f080d84c93c8666
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-05 14:56:39 +0545

    'formatted'

--- blog-app/src/components/RTE.jsx
+++ blog-app/src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
+        apiKey='your-api-key'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
